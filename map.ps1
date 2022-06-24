e0328c496c9bb47012248b035793e4b850689578
fabianohipolito <46888929+fabianohipolito@users.noreply.github.com>
2022-06-24 17:14:45 -0300

    Update map.ps1
    Ajuste

--- map.ps1
+++ map.ps1
@@ -1,7 +1,7 @@
 ﻿$net = new-object -ComObject WScript.Network
-$net.MapNetworkDrive("a:", "\\srv-soft01.paulista.local\Documentos\Suporte\Homologação Argentina", $false, "paulista\fabiano.oliveira", "Express@2024")
-$net.MapNetworkDrive("b:", "\\srv-soft01.paulista.local\Producao\SiTef\SiTef 5\CliSiTef - 5.0\Qualidade\Argentina\Unificada", $false, "paulista\fabiano.oliveira", "Express@2024")
-#$net.MapNetworkDrive("z:", "\\192.168.50.193\c$", $false, "paulista\fabiano.oliveira", "Express@2024")
+$net.MapNetworkDrive("a:", "\\srv-soft01.paulista.local\Documentos\Suporte\Homologação Argentina", $false, "dominio\login", "******")
+$net.MapNetworkDrive("b:", "\\srv-soft01.paulista.local\Producao\SiTef\SiTef 5\CliSiTef - 5.0\Qualidade\Argentina\Unificada", $false, "dominio\login", "******")
+#$net.MapNetworkDrive("z:", "\\192.168.50.193\c$", $false, "dominio\login", "******")
 
 
 
